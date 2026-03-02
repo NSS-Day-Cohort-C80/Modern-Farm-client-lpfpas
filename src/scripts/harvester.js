70e206138f8c5a894ae19edd4a41a16661a6bc05
Jude Andersen <jude.ottilie@gmail.com>
2026-03-02 09:57:09 -0600

    dave edits, debugging session

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,21 +2,23 @@
 
 export const harvestPlants = (plantsArray) => {
     const seedArray = []
-        for (const plants of plantsArray) {
-            if(plants.type === "Corn"){
-
-                plants.output = plants.output/2
-                    seedArray.push(plants)
-            }
-            else
-                {
-                let count = plants.output
-                while (count > 0) {
-                    seedArray.push(plants)
-                    count--
-                }
-            }
+    for (const plant of plantsArray) {
+        
+        let count = plant.output
+        if (plant.type === "Corn"){
+            count = plant.output/2;
         }
+/*
+        while (count > 0) {
+            seedArray.push(plant)
+            count = count-1;
+        }*/
+
+        for (let i=count; i>0; i--){
+            seedArray.push(plant);
+        }
+
+    }
     return seedArray
 }
 
